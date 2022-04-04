49962ef01f121f8a36746d727abfe6276d5ddb47
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-04-04 17:57:41 -0400

    Started working on compare_hero_and_villain function in SuperList view + Moved custom response formatting to it's own function

--- supers/views.py
+++ supers/views.py
@@ -10,21 +10,18 @@ class SuperList(APIView):
     def get(self, request):
         queryset = Super.objects.all()
         type = self.request.query_params.get('type')
+        hero = self.request.query_params.get('hero')
+        villain = self.request.query_params.get('villain')
 
         if type:
             type_filtered_queryset = queryset.filter(super_type__type = type)
             serializer = SuperSerializer(type_filtered_queryset, many=True)
             return Response(serializer.data)
 
-        custom_response = {'heroes': [], 'villains': []}
+        if hero and villain:
+            return(self.compare_hero_and_villain())
 
-        heroes = queryset.filter(super_type__type = 'hero')
-        villains = queryset.filter(super_type__type = 'villain')
-
-        custom_response['heroes'] = SuperSerializer(heroes, many=True).data
-        custom_response['villains'] = SuperSerializer(villains, many=True).data
-        
-        return Response(custom_response)
+        return Response(self.heroes_and_villains_response())
 
     def post(self, request):
         serializer = SuperSerializer(data=request.data)
@@ -32,9 +29,26 @@ class SuperList(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def heroes_and_villains_response(self):
+        supers = Super.objects.all()
+        heroes = supers.filter(super_type__type = 'hero')
+        villains = supers.filter(super_type__type = 'villain')
+
+        custom_response = {'heroes': [], 'villains': []}
+
+        custom_response['heroes'] = SuperSerializer(heroes, many=True).data
+        custom_response['villains'] = SuperSerializer(villains, many=True).data
+
+        return custom_response
+
+    def compare_hero_and_villain(self, hero_value, villain_value):
+        hero = Super.objects.get(name=hero_value)
+        villain = Super.objects.get(name=villain_value)
+
+        
 
 class SuperDetail(generics.RetrieveUpdateDestroyAPIView):
-    
+
     queryset = Super.objects.all()
     serializer_class = SuperSerializer
 

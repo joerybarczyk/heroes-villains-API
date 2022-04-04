2ea52a9f327783198a2683b2015aa42c6a624552
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-04-04 12:36:35 -0400

    Completed SuperDetail view w/ GET, PUT, DELETE functionality + Fixed import error in supers/urls.py

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Super
+from .serializers import SuperSerializer
 
 # Create your views here.
+class SuperList(generics.ListCreateAPIView):
+    pass
+
+class SuperDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Super.objects.all()
+    serializer_class = SuperSerializer

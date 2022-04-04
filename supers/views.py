2965cc941bf4ea51e8047f23b766156f02f834d6
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-04-04 12:52:50 -0400

    Added basic GET and POST functionality to SuperList view + Changed SuperSerializer to accept super_type_id in POST request body

--- supers/views.py
+++ supers/views.py
@@ -1,10 +1,21 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Super
 from .serializers import SuperSerializer
 
 # Create your views here.
-class SuperList(generics.ListCreateAPIView):
-    pass
+class SuperList(APIView):
+    def get(self, request):
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
 
 class SuperDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Super.objects.all()
